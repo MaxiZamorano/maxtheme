780aa61a03ca6074f47667292932a1cc5d9e9638
Maxi Zamorano <46273396+MaxiZamorano@users.noreply.github.com>
2023-04-14 16:55:23 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,73 +1,239 @@
 #!/bin/bash
 
-# Colores para el menú
+# Colores
 blanco='\033[1;37m'
 verde='\033[0;32m'
 amarillo='\033[1;33m'
 azul='\033[0;34m'
 magenta='\033[0;35m'
 cyan='\033[0;36m'
+gris='\033[1;30m'
+negro='\033[30m'
+rojo='\033[31m'
 reset='\033[0m'
 
 # Función para mostrar el menú
 show_menu() {
   clear
-  echo  "${blanco}=====================================${reset}"
-  echo  "${verde}            MENÚ PRINCIPAL${reset}"
-  echo  "${blanco}=====================================${reset}"
-  echo  "${azul}1) Instalar${reset}"
-  echo  "${amarillo}2) Actualizar${reset}"
-  echo  "${magenta}3) Desinstalar${reset}"
-  echo  "${cyan}4) Salir${reset}"
-  echo  "${blanco}=====================================${reset}"
+  echo ""
+  echo '                          ___  ___             _____ _'
+  echo '                          |  \/  |            |_   _| |'
+  echo '                          | .  . | __ ___  __   | | | |__   ___ _ __ ___   ___ '
+  echo '                          | |\/| |/ _  \ \/ /   | | |  _ \ / _ \  _   _ \ / _ \'
+  echo '                          | |  | | (_| |>  <    | | | | | |  __/ | | | | |  __/'
+  echo '                          \_|  |_/\__,_/_/\_\   \_/ |_| |_|\___|_| |_| |_|\___|'
+
+  echo ""
+  echo "                                          Max Theme para HestiaCP"
+  echo "                                                   1.0.0"
+  echo "                                           www.maxizamorano.com"
+  echo "                                      https://github.com/MaxiZamorano"
+  echo ""
+  echo  "${gris}____________________________________________________________________________________________________${reset}"
+  echo ""
+  echo  "${blanco}1) Instalar/Reinstalar${reset}"
+  echo  "${blanco}2) Desinstalar${reset}"
+  echo  "${blanco}3) Salir${reset}"
+  echo  "${gris}____________________________________________________________________________________________________${reset}"
+  echo ""
+
+
 }
 
+#función para instalar o reinstalar el tema
 instalar(){
 
         url="https://raw.githubusercontent.com/maxizamorano/maxtheme/main/themes/max-theme-light.css"
         dir_archivo="/usr/local/hestia/web/css/themes/custom/max-theme-light.css"
 
-        # Verifica si la carpeta "/root/custom" existe, si no la crea
+        url_2="https://raw.githubusercontent.com/maxizamorano/maxtheme/main/themes/max-theme-light-login.css"
+        dir_archivo_2="/usr/local/hestia/web/css/themes/max-theme-light-login.css"
+
+        # Verifica si la carpeta "/usr/local/hestia/web/css/themes/custom" existe, si no la crea
         if [ ! -d "/usr/local/hestia/web/css/themes/custom" ]; then
                 mkdir -p "/usr/local/hestia/web/css/themes/custom"
         fi
 
         # Descarga el archivo y sobrescribe si ya existe
         curl -s -o "$dir_archivo" -k "$url"
+        curl -s -o "$dir_archivo_2" -k "$url_2"
+
+        # Verifica si el archivo .copy-dark.min.css ya existe
+        if [ -f /usr/local/hestia/web/css/themes/.copy-dark.min.css ]; then
+              rm -rf /usr/local/hestia/web/css/themes/dark.min.css
+              cp /usr/local/hestia/web/css/themes/max-theme-light-login.css /usr/local/hestia/web/css/themes/dark.min.css
+              rm -rf /usr/local/hestia/web/css/themes/max-theme-light-login.css
+        else
+              cp /usr/local/hestia/web/css/themes/dark.min.css /usr/local/hestia/web/css/themes/.copy-dark.min.css
+              rm -rf /usr/local/hestia/web/css/themes/dark.min.css
+              cp /usr/local/hestia/web/css/themes/max-theme-light-login.css /usr/local/hestia/web/css/themes/dark.min.css
+              rm -rf /usr/local/hestia/web/css/themes/max-theme-light-login.css
+        fi
 
         archivo="/usr/local/hestia/web/css/themes/custom/max-theme-light.css"
         linea="5"
         css_inicio='    background-image: url("'
         css_final='");'
 
+        archivo_2="/usr/local/hestia/web/css/themes/dark.min.css"
+        linea_2="594"
+        css_inicio_2='    background-image: url("'
+        css_final_2='");'
 
-        echo "${blanco}=====================================${reset}"
+        linea_3="602"
+        css_inicio_3='    background-image: url("'
+        css_final_3='");'
+        css_color_fondo='    background: #f6f6f6;'
+
+        # Pregunta la URL del logo para el dashboard
+        echo  "${gris}____________________________________________________________________________________________________${reset}"
+        echo ""
+        echo "${amarillo}LOGO PARA LA BARRA SUPERIOR DEL DASHBOARD ${reset}"
+        echo ""
         echo "${cyan}Se recomienda que tu logo esté en formato ${amarillo}PNG, SVG o Webp${reset}"
-        echo "${cyan}Utiliza un formato de URL correcto y en https, por ejmplo: ${amarillo}https://example.com/img/mi-logo.png${reset}"
-        echo "${blanco}=====================================${reset}"
+        echo "${cyan}Utiliza un formato de URL correcto y en https, por ejmplo: ${amarillo}https://example.com/img/tu-logo.png${reset}"
+        echo  "${gris}____________________________________________________________________________________________________${reset}"
+        echo ""
         echo -n "${verde}Pega o escribe la URL de tu logo: ${reset}"
-        read texto
+        read url_logo
 
-        url=$(echo $texto | sed 's/\//\\\//g')
+        url=$(echo $url_logo | sed 's/\//\\\//g')
 
         sed -i "${linea}s/.*/$css_inicio${url}$css_final/" $archivo
 
+        # Pregunta la URL del logo para el login
+        echo  "${gris}____________________________________________________________________________________________________${reset}"
+        echo ""
+        echo "${amarillo}LOGO PARA LA PANTALLA DE LOGIN O INICIO DE SESIÓN ${reset}"
+        echo ""
+        echo "${cyan}Se recomienda que tu logo esté en formato ${amarillo}PNG, SVG o Webp${reset}"
+        echo "${cyan}Utiliza un formato de URL correcto y en https, por ejmplo: ${amarillo}https://example.com/img/tu-logo.png${reset}"
+        echo  "${gris}____________________________________________________________________________________________________${reset}"
+        echo ""
+        echo -n "${verde}Pega o escribe la URL de tu logo: ${reset}"
+        read url_logo_login
 
-        echo  "${blanco}=====================================${reset}"
-        echo  "${cyan}   ¡TEMA INSTALADO CORRECTAMENTE!${reset}"
-        echo  "${blanco}=====================================${reset}"
+        url_2=$(echo $url_logo_login | sed 's/\//\\\//g')
 
-        exit;
+        sed -i "${linea_2}s/.*/$css_inicio_2${url_2}$css_final_2/" $archivo_2
+
+        # Pregunta la URL del Fondo
+        while true; do
+        echo  "${gris}____________________________________________________________________________________________________${reset}"
+        echo ""
+        echo -n "${verde}¿Quieres agregar una imagen de fondo en el inicio de sesión? [S/N]:${reset} "
+        read respuesta
+
+        case "$respuesta" in
+          [Ss]* )
+            echo  "${gris}____________________________________________________________________________________________________${reset}"
+            echo ""
+            echo "${amarillo}IMAGEN DE FONDO PARA LA PANTALLA DE LOGIN O INICIO DE SESIÓN ${reset}"
+            echo ""
+            echo "${cyan}Se recomienda que la imagen tenga una resolución de ${amarillo}1920x1080px ${cyan}y esté en formato ${amarillo}PNG, JPG o Webp${reset}"
+            echo "${cyan}Utiliza un formato de URL correcto y en https, por ejmplo: ${amarillo}https://example.com/img/tu-fondo.png${reset}"
+            echo  "${gris}____________________________________________________________________________________________________${reset}"
+            echo ""
+            echo -n "${verde}Pega o escribe la URL de la imagen de fondo: ${reset}"
+            read url_img_fondo
+
+            url_3=$(echo $url_img_fondo | sed 's/\//\\\//g')
+            sed -i "${linea_3}s/.*/$css_inicio_3${url_3}$css_final_3/" $archivo_2
+
+            echo  "${gris}____________________________________________________________________________________________________${reset}"
+            echo ""
+            echo "${verde}                                       ¡TEMA INSTALADO CORRECTAMENTE! ${reset}"
+            echo  "${gris}____________________________________________________________________________________________________${reset}"
+            echo ""
+
+            exit
+            ;;
+          [Nn]* )
+            sed -i "${linea_3}s/.*/$css_color_fondo/" $archivo_2
+
+            echo  "${gris}____________________________________________________________________________________________________${reset}"
+            echo ""
+            echo "${verde}                                       ¡TEMA INSTALADO CORRECTAMENTE! ${reset}"
+            echo  "${gris}____________________________________________________________________________________________________${reset}"
+            echo ""
+
+            exit
+            ;;
+          * )
+            echo ""
+            echo "${rojo}La respuesta es inválida, debes responder con "S/N o s/n".${reset}"
+            ;;
+        esac
+        done
+       
 }
 
 
-# Loop principal del script
+#función para desinstalar el tema
+desinstalar(){
+       # Pregunta si quiere desinstalar el tema
+        while true; do
+        echo  "${gris}____________________________________________________________________________________________________${reset}"
+        echo ""
+        echo "${cyan}NOTA:${reset} Si lo desinstalas restauraremos los archivos originales y se activará el tema por defecto de HestiaCP."
+        echo ""
+        echo -n "${verde}¿Quieres desinstalar el tema? [S/N]:${reset} "
+        read respuesta
+
+        case "$respuesta" in
+          [Ss]* )
+
+          # Verifica si está instalado el tema actualmente
+          if [ -f /usr/local/hestia/web/css/themes/.copy-dark.min.css ]; then
+            # Elimina los archivos y restaura el original
+            rm -rf /usr/local/hestia/web/css/themes/dark.min.css
+            cp /usr/local/hestia/web/css/themes/.copy-dark.min.css /usr/local/hestia/web/css/themes/dark.min.css
+            rm /usr/local/hestia/web/css/themes/.copy-dark.min.css
+            rm -rf /usr/local/hestia/web/css/themes/custom/max-theme-light.css
+
+            echo  "${gris}____________________________________________________________________________________________________${reset}"
+            echo ""
+            echo "${verde}                                     ¡TEMA DESINSTALADO CORRECTAMENTE! ${reset}"
+            echo  "${gris}____________________________________________________________________________________________________${reset}"
+            echo ""
+
+            exit
+          else
+            echo ""
+            echo "${rojo}¡No tienes el tema instalado! Solo podemos desinstalarlo si el tema fue instalado anteriormente.${reset}" 
+            echo ""
+            exit
+          fi
+            
+
+            ;;
+          [Nn]* )
+            # No desinstala el tema
+            echo  "${gris}____________________________________________________________________________________________________${reset}"
+            echo ""
+            echo "${verde}                                      ¡GENIAL! CONTINÚAS CON TU TEMA ${reset}"
+            echo  "${gris}____________________________________________________________________________________________________${reset}"
+            echo ""
+
+            exit
+            ;;
+          * )
+            echo ""
+            echo "${rojo}La respuesta es inválida, debes responder con "S/N o s/n".${reset}"
+            ;;
+        esac
+        done
+
+}
+
+
+# Loop o bucle para respuesta del usuario
 while true; do
   # Mostrar el menú
   show_menu
 
   # Leer la opción seleccionada por el usuario
-  echo -n "${verde}Seleccione una opción [1-4]: ${reset}"
+  echo -n "${verde}Selecciona una opción [1-3]: ${reset}"
   read opcion
 
   # Evaluar la opción seleccionada
@@ -76,20 +242,14 @@ while true; do
       instalar
       ;;
     2)
-      echo "Ha seleccionado la Opción 2"
-      # Agrega aquí el código para la Opción 2
+      desinstalar
       ;;
     3)
-      echo "Ha seleccionado la Opción 3"
-      # Agrega aquí el código para la Opción 3
-      ;;
-    4)
       exit
       ;;
     *)
-      echo "${cyan}Opción inválida. Intente de nuevo.${reset}"
+      echo "${rojo}Opción inválida. Intente de nuevo.${reset}"
       sleep 3
       ;;
   esac
-  read -n 1 -s -r -p "Presione cualquier tecla para continuar..."
 done
