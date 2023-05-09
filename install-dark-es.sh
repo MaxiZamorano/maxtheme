31505705e223ea0e7d4ca3d5e3373589699174c5
Maxi Zamorano <46273396+MaxiZamorano@users.noreply.github.com>
2023-05-08 22:12:20 -0400

    Update install-dark-es.sh

--- install-dark-es.sh
+++ install-dark-es.sh
@@ -29,7 +29,7 @@ show_menu() {
   
   echo ""
   echo "                                       Max Theme Dark para HestiaCP"
-  echo "                                                   v2.0"
+  echo "                                                   v2.1"
   echo "                                           www.maxizamorano.com"
   echo "                                      https://github.com/MaxiZamorano"
   echo ""
